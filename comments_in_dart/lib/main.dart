356d9e9e4d4ccf274b2430f64822f7eb51c3c019
ShekAdnan <adnansk418@gmail.com>
2021-12-21 19:55:49 +0600

    Comments-in-dart

---
+++ comments_in_dart/lib/main.dart
@@ -0,0 +1,20 @@
+// Comments will be helpfull for developers who will read this code
+
+/*
+ multiline comment but it is  discourage in dart
+ */
+
+void main (){
+  
+  // This is my first line of code , whatever you write after two forward slash the compiler will egnore  it
+  print ('Hello World'); //  this is another comment ....
+  
+  print  ('This is my first application');
+  
+  
+  // Performing arithmetic operation
+  print (12/4);
+  
+  // Printing out boolean value
+  print (false);
+}
